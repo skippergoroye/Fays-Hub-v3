92c4c56c6ca97836bedbd98bc81d23c9865a23c6
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-04-19 20:27:43 +0100

    feat: image picker added here

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,43 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import assets from '@/assets/upload_area.png';
 
 
 const Admin = () => {
+  const [files, setFiles] = useState<File[]>([]); 
   
 
 
 
 
   return (
- <div>hhhhhhh</div>
+ <div className='px-6 py-8'>
+  <div className="flex items-center gap-3 mt-2">
+  <label htmlFor="imageUpload">
+    <input
+      onChange={(e) => {
+        const selectedFile = e.target.files?.[0];
+        if (selectedFile) {
+          setFiles([selectedFile]);
+        }
+      }}
+      type="file"
+      id="imageUpload"
+      hidden
+      accept="image/*"
+    />
+    <Image
+      className="max-w-24 cursor-pointer"
+      src={files[0] ? URL.createObjectURL(files[0]) : assets}
+      alt="Upload"
+      width={100}
+      height={100}
+    />
+  </label>
+</div>
+
+ </div>
   )
 }
 
